a1cd90f9f8ec39b1e2363d92e823641b2bf1fbf8
raulsperoni <rsperoni@expertinstitute.com>
2025-03-04 17:52:23 -0300

    .

--- core/tests/test_basic.py
+++ core/tests/test_basic.py
@@ -13,6 +13,7 @@ def test_homepage_status(client):
     response = client.get(url)
     assert response.status_code == 200
 
+@pytest.mark.django_db
 def test_admin_login_page(client):
     """Test that the admin login page is accessible."""
     url = reverse('admin:login')
